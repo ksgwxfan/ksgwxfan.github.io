60cd3daa2b5fd3aeb3a1425d82c09bb43135fcb4
Kyle Gentry <36864779+ksgwxfan@users.noreply.github.com>
2026-09-15 14:38:31 -0400

    updated license to CC BY-NC-ND

--- 5day/7day_canvas_populate.js
+++ 5day/7day_canvas_populate.js
@@ -1,10 +1,10 @@
-/**
- * Copyright (c) 2026, Kyle Gentry (github.com/ksgwxfan)
- *
- * This work is licensed under the Creative Commons Attribution-NoDerivatives 4.0 
- * International License. To view a copy of this license, visit 
- * http://creativecommons.org or send a letter to 
- * Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+/*!
+Copyright (c) 2026, Kyle Gentry (github.com/ksgwxfan)
+
+This work is licensed under the Creative Commons Attribution-NonCommercial
+-NoDerivatives 4.0 International License. To view a copy of this license, visit
+https://creativecommons.org/licenses/by-nc-nd/4.0/deed.en or send a letter to
+Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
  */
 
 function populate_options() {
